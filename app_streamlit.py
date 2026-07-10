ee3468c275f1b7003352651d08c618727f503be2
Sandy Yoga Prakasa Holley <holleysandyyogaprakasa@gmail.com>
2026-07-10 18:02:14 +0700

    Update file input method to file uploader
    Replaced file_input with file_uploader for better functionality.

--- app_streamlit.py
+++ app_streamlit.py
@@ -12,7 +12,8 @@ st.subheader("Studi Kasus: Deteksi Barang Bawaan (Backpack/Luggage)")
 model_v8 = YOLO('best_yolov8_final.pt')
 model_v11 = YOLO('best_yolov11_final.pt')
 
-uploaded_file = st.file_input("Upload Foto Uji", type=["jpg", "jpeg", "png"])
+# Sudah diperbaiki menjadi st.file_uploader
+uploaded_file = st.file_uploader("Upload Foto Uji", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     # Simpan sementara
@@ -23,18 +24,18 @@ if uploaded_file is not None:
     col1, col2, col3 = st.columns(3)
     
     with col1:
-        st.image(img, caption="Foto Asli", use_column_width=True)
+        st.image(img, caption="Foto Asli", use_container_width=True)
         
     with col2:
         start = time.time()
         res_v8 = model_v8(img_path, conf=0.50)
         t_v8 = round((time.time() - start) * 1000, 2)
         res_v8[0].save("v8_out.jpg")
-        st.image("v8_out.jpg", caption=f"Hasil YOLOv8 ({t_v8} ms)", use_column_width=True)
+        st.image("v8_out.jpg", caption=f"Hasil YOLOv8 ({t_v8} ms)", use_container_width=True)
         
     with col3:
         start = time.time()
         res_v11 = model_v11(img_path, conf=0.50)
         t_v11 = round((time.time() - start) * 1000, 2)
         res_v11[0].save("v11_out.jpg")
-        st.image("v11_out.jpg", caption=f"Hasil YOLO11 ({t_v11} ms)", use_column_width=True)
+        st.image("v11_out.jpg", caption=f"Hasil YOLO11 ({t_v11} ms)", use_container_width=True)
